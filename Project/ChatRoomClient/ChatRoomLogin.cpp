6ee987cf76e395693259ce5bf04bf06000ce49a5
小熊熊熊 <981303787@qq.com>
2024-08-05 22:19:27 +0800

    完成部分登录界面

--- Project/ChatRoomClient/ChatRoomLogin.cpp
+++ Project/ChatRoomClient/ChatRoomLogin.cpp
@@ -1,49 +1,85 @@
 ﻿#include "ChatRoomLogin.h"
+#include <QMessageBox>
 
 ChatRoomLogin::ChatRoomLogin(QWidget * parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
 
-    this->setWindowFlags(Qt::SplashScreen | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);//WindowStaysOnTopHint窗口顶置
+    //WindowStaysOnTopHint窗口顶置
+    this->setWindowFlags(Qt::SplashScreen | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
 
     const auto backSize = ui.m_labAnimation->size();
     m_ptrBack = new QMovie("./res/login/back.gif");
     m_ptrBack->setScaledSize(backSize);
-
     ui.m_labAnimation->setMovie(m_ptrBack);
     m_ptrBack->start();
 
+    QIcon ico("./res/login/logo.ico");
+    QPixmap logo = ico.pixmap(QSize(73, 47));
+    ui.m_labLogo->setPixmap(logo);
+    ui.m_labLogo->resize(logo.width(), logo.height());
+    ui.m_labLogo->show();
 
-    const auto & wndSize = this->size();
-
-    QImage * img = new QImage;
-    if (img->load("./res/login/profile.png"))
+    m_imgLogin = new QImage;
+    if (m_imgLogin->load("./res/login/profile.png"))
     {
-        auto b = ui.m_labProfile->size();
-        QImage c = img->scaled(b.width(), b.height(), Qt::KeepAspectRatio);
+        auto labSize = ui.m_labProfile->size();
+        QImage c = m_imgLogin->scaled(labSize.width(), labSize.height(), Qt::KeepAspectRatio);
         ui.m_labProfile->setPixmap(QPixmap::fromImage(c));
     }
 
-    ui.m_chkAutoLogin->setStyleSheet("QCheckBox{ "
-                                     " color: rgb(168, 168, 168);}"
-                                     "QCheckBox::indicator {"
-                                     " border: 1px solid #A8A8A8; "
-                                     "}");
-    ui.m_chkRemPasswd->setStyleSheet("QCheckBox{ "
-                                     " color: rgb(168, 168, 168);}"
-                                     " QCheckBox::indicator {"
-                                     " border: 1px solid #A8A8A8; "
-                                     " color: rgb(168, 168, 168);}"
-                                     "}");
-    ui.m_chkRemPasswd->setChecked(true);
+    ui.m_btnSetting->setToolTip(QStringLiteral("设置"));
+    ui.m_btnMin->setToolTip(QStringLiteral("最小化"));
+    ui.m_btnClose->setToolTip(QStringLiteral("关闭"));
 
-    ui.m_btnLogin->setStyleSheet("QPushButton {"
-                                 "  border-radius: 3px;"
-                                 "  background-color: rgb(7, 189, 253);"
-                                 "  color: rgb(255, 255, 255);"
-                                 "}");
+    ui.m_chkRemPasswd->setChecked(true);
+    connect(ui.m_btnLogin, SIGNAL(clicked()), this, SLOT(onBtnClickedLogin()));
+    connect(ui.m_btnRegister, SIGNAL(clicked()), this, SLOT(onBtnClickedRegister()));
+    connect(ui.m_btnClose, SIGNAL(clicked()), this, SLOT(onBtnClickedClose()));
+
 }
 
 ChatRoomLogin::~ChatRoomLogin()
 {}
+
+void ChatRoomLogin::mousePressEvent(QMouseEvent * event)
+{
+    m_blPressed = true; // 当前鼠标按下的即是QWidget而非界面上布局的其它控件
+    last = event->globalPos();
+}
+
+void ChatRoomLogin::mouseMoveEvent(QMouseEvent * event)
+{
+    if (m_blPressed)
+    {
+        int dx = event->globalX() - last.x();
+        int dy = event->globalY() - last.y();
+        last = event->globalPos();
+        move(x() + dx, y() + dy);
+    }
+}
+
+void ChatRoomLogin::mouseReleaseEvent(QMouseEvent * event)
+{
+    int dx = event->globalX() - last.x();
+    int dy = event->globalY() - last.y();
+    move(x() + dx, y() + dy);
+    m_blPressed = false; // 鼠标松开时，置为false
+}
+
+void ChatRoomLogin::onBtnClickedClose()
+{
+    QApplication * app;
+    app->quit();
+}
+
+void ChatRoomLogin::onBtnClickedLogin()
+{
+    QMessageBox::information(this, QStringLiteral("提示"), QStringLiteral("登录！"), QMessageBox::NoButton);
+}
+
+void ChatRoomLogin::onBtnClickedRegister()
+{
+    QMessageBox::information(this, QStringLiteral("提示"), QStringLiteral("注册账号！"), QMessageBox::NoButton);
+}
--- Project/ChatRoomClient/ChatRoomLogin.h
+++ Project/ChatRoomClient/ChatRoomLogin.h
@@ -2,6 +2,7 @@
 
 #include <QWidget>
 #include <QMovie>
+#include <QMouseEvent>
 #include "ui_ChatRoomLogin.h"
 
 class ChatRoomLogin : public QWidget
@@ -12,8 +13,24 @@ public:
     ChatRoomLogin(QWidget * parent = nullptr);
     ~ChatRoomLogin();
 
+    void mousePressEvent(QMouseEvent * event);//鼠标点击
+    void mouseMoveEvent(QMouseEvent * event);//鼠标移动
+    void mouseReleaseEvent(QMouseEvent * event);//鼠标释放
+
 private:
     Ui::ChatRoomLoginClass ui;
 
     QMovie * m_ptrBack = nullptr;
+
+    QImage * m_imgLogin = nullptr;
+
+    QPoint last;
+    bool m_blPressed = false;
+
+private slots:
+    void onBtnClickedClose();
+
+    void onBtnClickedLogin();
+
+    void onBtnClickedRegister();
 };
